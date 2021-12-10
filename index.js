c33efe30cbbc2c18f8bfc76938b147b982f40b1e
Susana Marquina <smarq86@gmail.com>
2021-12-09 20:54:10 -0800

    added new card button and function

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-let firstCard = 12;
+let firstCard = 2;
 let secondCard = 11;
+cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackjack = false;
 let isAlive = true;
-let message = "";
+let message = '';
+let messageEl = document.getElementById('message-el');
+let sumEl = document.getElementById('sum-el');
+let cardsEl = document.getElementById('cards-el');
 
 function startGame() {
+  renderGame();
+}
+
+function renderGame() {
+  cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+  sumEl.textContent = 'Sum: ' + sum;
   if (sum < 21) {
     message = 'Do you want to draw another card?';
   } else if (sum === 21) {
-    message = "Woohoo! You've won!";
+    message = "You've got Blackjack.";
     hasBlackjack = true;
   } else {
-    message = 'Sorry, you lost!';
+    message = 'Sorry, you lost.';
     isAlive = false;
   }
-  console.log(message)
+  messageEl.textContent = message;
 }
 
+function newCard() {
+  let card = 4;
+  sum += card;
+  renderGame();
+}
