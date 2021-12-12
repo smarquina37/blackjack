7ce772a53dde55861aef5522bca892ba928c7bcc
Susana Marquina <smarq86@gmail.com>
2021-12-12 13:04:12 -0800

    added player object

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let player = {
+  name: "Susana",
+  chips: 145
+}
+let cards = [];
+let sum = 0;
 let hasBlackjack = false;
-let isAlive = true;
+let isAlive = false;
 let message = '';
 let messageEl = document.getElementById('message-el');
 let sumEl = document.getElementById('sum-el');
 let cardsEl = document.getElementById('cards-el');
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $ " + player.chips;
 
 function getRandomCard() {
-  return Math.floor(Math.random() * 13) + 1;
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
 }
 
 function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
   renderGame();
 }
 
@@ -37,8 +54,10 @@ function renderGame() {
 }
 
 function newCard() {
-  let card = getRandomCard();
-  sum += card;
-  cards.push(card);
-  renderGame();
+  if (isAlive === true && hasBlackjack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
 }
